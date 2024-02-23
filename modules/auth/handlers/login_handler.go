ba247a631d062af3dbecc296d86035b85fc51744
richardktran.dev <richardktran.dev@gmail.com>
2024-02-24 00:52:27 +0700

    change transports to handlers

--- modules/auth/handlers/login_handler.go
+++ modules/auth/handlers/login_handler.go
@@ -1,4 +1,4 @@
-package transports
+package handlers
 
 import (
 	"github.com/gin-gonic/gin"
--- routes/api.go
+++ routes/api.go
@@ -2,7 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	todoHandler "github.com/richardktran/MyBlogBE/modules/todo-list/transports"
+	todoHandler "github.com/richardktran/MyBlogBE/modules/todo-list/handlers"
 )
 
 func V1(router *gin.RouterGroup) {
